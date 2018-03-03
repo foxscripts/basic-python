a1e8bc6a88a40141b28304c2b88ba0b76ad49821
foxscripts <vibhormehta777@gmail.com>
2018-03-03 22:01:11 +0530

    convert char to ASCII value

---
+++ ascii.py
@@ -0,0 +1,3 @@
+# py script to print the ASCII value of input key
+inputKey = raw_input("\n\n\tEnter a key whose ASCII value you want: ")
+print "\n\n\tThe ASCII value of",inputKey,"is",ord(inputKey),"\n\n"
