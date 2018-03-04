5fa2d72cff258799a0f4caabc500be8dbf1c15fa
foxscripts <vibhormehta777@gmail.com>
2018-03-04 10:54:41 +0530

    updated

--- ascii.py
+++ ascii.py
@@ -1,3 +1,11 @@
 # py script to print the ASCII value of input key
-inputKey = raw_input("\n\n\tEnter a key whose ASCII value you want: ")
-print "\n\n\tThe ASCII value of",inputKey,"is",ord(inputKey),"\n\n"
+
+def ascii():
+    inputKey = raw_input("\n\n\tEnter a key whose ASCII value you want: ")
+    print "\n\n\tThe ASCII value of",inputKey,"is",ord(inputKey),"\n\n"
+ascii()
+choice = raw_input("\tDo you want to continue (y) ")
+if(choice == 'y'):
+    ascii()
+else:
+    print "\n\n\tGoodBye :)"
