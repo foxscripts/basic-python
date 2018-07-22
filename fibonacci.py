106d7c38169b07491b5bb19f9dddacaddb1e8602
foxscripts <vibhormehta777@gmail.com>
2018-07-22 11:47:09 +0530

    updated

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,11 @@
-var1 , var2 = 0 , 1
-while var2 < 100:
-	print(var2)
-	var1 , var2 = var2 , var1+var2
+# error handling
+try:
+	var1 , var2 = 0 , 1
+	while var2 < 100:
+		print(var2)
+		var1 , var2 = var2 , var1+var2
+
+except Exception as e:
+	print(e)	
+
+
